d3f6d289e78ec419038348956645ef99117999f4
MasameEh <samehms116@gmail.com>
2023-04-16 15:34:23 +0200

    last changes
    Signed-off-by: MasameEh <samehms116@gmail.com>

--- algorithms/InsertionSort.java
+++ algorithms/InsertionSort.java
@@ -1,41 +1,33 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class InsertionSort
 {
     void insertionSort(int arr[])
     {
-        int n = arr.length;
+        int size = arr.length;
 
-        for(int i = 1; i < n; i++)
+        for(int i = 1; i < size; i++)
         {
-            int key = arr[i];
+            int temp = arr[i];
             int j = i - 1;
-            while(j >= 0 && arr[j] > key)
+            while(j >= 0 && arr[j] > temp)
             {
                 arr[j + 1] = arr[j];
                 j = j - 1;
             }
-            arr[j + 1] = key;
+            arr[j + 1] = temp;
         }
     }
  
-    
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
- 
-        System.out.println();
-    }
- 
+
     public static void main(String args[])
     {
-        int arr[] = { 12, 11, 13, 5, 6 };
- 
+        int arr[] = { 44, 1, 13, 9, 6 };
         InsertionSort ob = new InsertionSort();
         ob.insertionSort(arr);
- 
-        printArray(arr);
+        
+        System.out.println(Arrays.toString(arr));
     }
 };
